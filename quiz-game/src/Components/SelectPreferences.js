ab4d99a968d806f1fae64cdfccd7c6ddb61c71c4
Shreyas Viswanathan <shrevis2018@gmail.com>
2023-06-29 13:53:18 -0500

    style updates

--- quiz-game/src/Components/SelectPreferences.js
+++ quiz-game/src/Components/SelectPreferences.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function SelectPreferences({
+  dark,
   difficulty, 
   difficulties, 
   setDifficulty, 
@@ -13,35 +14,50 @@ export default function SelectPreferences({
   questionType,
   setQuestionType
 }) {
+
+  const labelStyle = {
+    color: dark ? "#fff" : "#000"
+  }
+
   return (
-    <div className = "preferences">
-        <label htmlFor="difficulty">Choose a difficulty: </label>
+    <div className = "preferences" style = {{backgroundColor: dark ? "#000" : "#fff"}}>
+      <div className = "input-group">
+      <label htmlFor="difficulty" style = {labelStyle}>Choose a difficulty: </label>
         <select name = "difficulty" id = "difficulty" value = {difficulty} onChange = {setDifficulty}>
           {difficulties.map((option, idx) => {
             return <option key = {idx} value = {option}>{option.charAt(0).toUpperCase() + option.slice(1)}</option>
           })}
         </select>
-        <label htmlFor="category">Choose a category: </label>
+      </div>
+      <div className = "input-group">
+      <label htmlFor="category" style = {labelStyle}>Choose a category: </label>
         <select name = "category" id = "category" value = {selectedCategory.category} onChange = {setCategory}>
           {Object.keys(categories).map((category, index) => {
             return <option key = {index} value={category}>{category}</option>
           })}
         </select>
-        <label htmlFor="numQuestions">Choose number of questions: </label>
+      </div>
+      <div className = "input-group">
+      <label htmlFor="numQuestions" style = {labelStyle}>Choose number of questions: </label>
         <select name = "numQuestions" id = "numQuestions" value = {numQuestions} onChange={setNumQuestions}>
           {[5,10,15,20].map((amount, i) => {
             return <option key = {i} value = {amount}>{amount}</option>
           })}
         </select>
-        <label htmlFor="questionType">Choose question type: </label>
+      </div>
+      <div className = "input-group">
+      <label htmlFor="questionType" style = {labelStyle}>Choose question type: </label>
         <select name = "questionType" id = "questionType" value = {questionType} onChange={setQuestionType}>
           {["Any Type", "Multiple Choice", "True / False"].map((type, id) => {
             return <option key = {id} value = {type}>{type}</option>
           })}
         </select>
-        <button onClick = {begin}>
+      </div>
+      <div className = "input-group">
+      <button onClick = {begin} className = "start-button">
             Start Quiz
         </button>
+      </div>
     </div>
   )
 }
